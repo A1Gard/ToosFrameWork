f5917278f773e9291df957f4149ca6c0b23c6aae
A1Gard <info@pxt.ir>
2016-03-07 11:32:01 +0330

    update install -> [javascript] notification optimize

--- src/install/assets/js/install.js
+++ src/install/assets/js/install.js
@@ -1,6 +1,18 @@
-function validateEmail(email) {
+
+// email validtor 
+function ValidateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+// add notifi
+function NotifNow(text, mode) {
+    var el = "<div class='notification "+mode+"' > "+text+" </div>" ;
+    $(".notification-bar").append( el );
+    setTimeout(function () {
+        $(".notification-bar .notification").slideUp(900, function () {
+            $(".notification-bar .notification;").remove();
+        });
+    }, 6000);
 }
 
 $(function () {
@@ -10,16 +22,12 @@ $(function () {
         $.post('ajax/dbCheker.php', $("#install-form").serialize(), function (e) {
             $("#dbchecker").removeAttr('disabled');
             if ($.trim(e) == 'true') {
+                
                 $(".notification-bar").append("<div class='notification success' > Database connected</div>");
                 $("#submit").show(300);
             } else {
 //                alert(e);
-                $(".notification-bar").append("<div class='notification error' > " + e + " </div>");
-                setTimeout(function () {
-                    $(".notification-bar .error").hide(900, function () {
-                        $(".notification-bar .error").remove();
-                    });
-                }, 6000);
+                NotifNow(e,'error');
                 $("#submit").hide(300);
             }
         });
@@ -30,13 +38,8 @@ $(function () {
         $(this).attr('disabled', 'disabled');
         $.get('ajax/configChecker.php', function (e) {
             $("#configchecker").removeAttr('disabled');
-
-            $(".notification-bar").append("<div class='notification error' > " + e + " </div>");
-            setTimeout(function () {
-                $(".notification-bar .error").hide(900, function () {
-                    $(".notification-bar .error").remove();
-                });
-            }, 6000);
+            
+            NotifNow(e,'error');
             $("#submit").hide(300);
 
         });
@@ -46,63 +49,65 @@ $(function () {
 
     $("#nfocheck").bind('click', function () {
         $("#info-form input").removeClass('req-error');
-        var elm =  "#manager_username";
-        var  b = true ;
-        elm =  "#manager_username";
+        var elm = "#manager_username";
+        var b = true;
+
+
+        // username checker
+        elm = "#manager_username";
         if ($(elm).val().length < 3) {
+            NotifNow("Username is too short.",'error');
             $(elm).addClass('req-error');
             b = false;
         }
-        
-        elm =  "#manager_password";
+        // password checker
+        elm = "#manager_password";
         if ($(elm).val().length < 6) {
+            NotifNow("Password is too short.",'error');
             $(elm).addClass('req-error');
             b = false;
         }
-        elm =  "#manager_password2";
+        // password reapt chcker 
+        elm = "#manager_password2";
         if ($(elm).val().length < 6) {
+            NotifNow("Repeat password is too short.",'error');
             $(elm).addClass('req-error');
             b = false;
         }
-        
-        elm =  "#email";
+
+        // email checker
+        elm = "#email";
         if ($(elm).val().length < 6) {
+            NotifNow("Email is too short.",'error');
             $(elm).addClass('req-error');
             b = false;
         }
-        
-        if (!validateEmail($(elm).val())) {
+        // email validator
+        if (!ValidateEmail($(elm).val())) {
+            NotifNow("email is invalid.",'error');
             $(elm).addClass('req-error');
             b = false;
-             $(".notification-bar").append("<div class='notification error' > Invalid email. </div>");
-            setTimeout(function () {
-                $(".notification-bar .error").hide(900, function () {
-                    $(".notification-bar .error").remove();
-                });
-            }, 6000);
+
         }
+        // check is two password is equal or not
         if ($("#manager_password").val() !== $("#manager_password2").val()) {
-             $(".notification-bar").append("<div class='notification error' > Repeated password is not equal. </div>");
-            setTimeout(function () {
-                $(".notification-bar .error").hide(900, function () {
-                    $(".notification-bar .error").remove();
-                });
-            }, 6000);
+            NotifNow('Repeated password is not equal with orginal password.','error');
             b = false;
         }
-        
-        
-        elm =  "#title";
+
+        // title checker
+        elm = "#title";
         if ($(elm).val().length < 3) {
+            NotifNow("title is too short.",'error');
             $(elm).addClass('req-error');
             b = false;
         }
-        
+
         if (!b) {
             return false;
         }
-        
+
         $("#info-form").submit();
-        
+
     });
 });
