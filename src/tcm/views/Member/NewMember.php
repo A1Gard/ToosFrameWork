f8c5b42d21eabd08dea12e284bdcd391ff8de94d
A1Gard <info@pxt.ir>
2016-03-24 02:08:48 +0430

    change member list UI

--- src/tcm/views/Member/NewMember.php
+++ src/tcm/views/Member/NewMember.php
@@ -5,26 +5,30 @@ $frm->AddField('text', 'نام', null, array('name' => 'member_name'));
 $frm->AddField('email', 'ایمیل', null, array('name' => 'member_email'));
 $frm->AddField('password', 'گذرواژه', null, array('name' => 'member_password'));
 $frm->AddField('text', 'استاتوس', null, array('name' => 'member_status'));
-$frm->AddField('text', 'زمان تمدید', null, array('name' => 'member_active_time','class'=>  _lg('datepicker')));
+$frm->AddField('text', 'زمان تمدید', null, array('name' => 'member_active_time', 'class' => _lg('datepicker')));
 $frm->AddField('file', 'آواتار', null, array('name' => 'member_avatar'));
 $frm->AddField('text', 'مقطع', null, array('name' => 'member_degree'));
 $frm->AddField('text', 'رشته تحصیلی', null, array('name' => 'member_field'));
 $frm->AddField('text', 'شماره تماس', null, array('name' => 'member_number'));
 $frm->AddField('text', 'شهر', null, array('name' => 'member_city'));
-$frm->AddField('select', 'نوع', null, array('name' => 'member_type'), 
-        array(0 => array('0', 'تایید نشده'), 
-              1 => array('1', 'تایید شده'),
-              2 => array('2','اخراجی')));
+$frm->AddField('select', 'نوع', null, array('name' => 'member_type'), array(0 => array('0', 'تایید نشده'),
+    1 => array('1', 'تایید شده'),
+    2 => array('2', 'اخراجی')));
 
 $frm->AddField('submit', '', 'ارسال');
 ?>
 <br />
 <h2 class="rtl">
-    <?php echo  $this->title ?>
+    <?php echo $this->title ?>
 </h2>
 <br />
 <br />
-<?php 
-    $frm->Render(); 
-?>
-        
+<div class="row">
+    <div class="grd-primary">
+        <?php
+        $frm->Render();
+        ?>
+
+    </div>
+</div>
+
