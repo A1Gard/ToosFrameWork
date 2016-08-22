2e9aa7d49dffeb7b200bbf6b23753f8ee4f343aa
A1Gard <info@pxt.ir>
2016-08-22 10:23:32 +0430

    Fix search and filter bug [php]

--- src/libs/TPagination.php
+++ src/libs/TPagination.php
@@ -6,23 +6,24 @@
  * @date : 09-Juan-2014 
  * @time : 16:32 
  * @subpackage   TPagination
- * @version 1.0
- * @todo : crsate pagination
+ * @version 1.1
+ * @todo : create pagination
  */
 class TPagination {
 
     // page count need to be render 
     private $page_count = 1;
+    public $allow_in_next_pages = array('search', 'filter', 'fields', 'rel');
 
     /**
      * create pagination
      * @param int $page_count page count
      */
     function __construct($page_count = 1) {
-        
+
         // pre hook
         _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $page_count);
-        
+
         // ste page count
         $this->page_count = $page_count;
     }
@@ -36,16 +37,14 @@ class TPagination {
         // pre hook
         _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $acvtive_page);
 
-		$result = $this->RenderX($acvtive_page);
-        
+        $result = $this->RenderX($acvtive_page);
+
         // result hook
         _hk('R' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $result);
-        
+
         echo $result;
     }
-    
-    
-    
+
     /**
      * render pagonation
      * @param int $acvtive_page
@@ -53,10 +52,10 @@ class TPagination {
     public function RenderX($acvtive_page = '') {
 
 
-        
+
         // pre hook
         _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $acvtive_page);
-        
+
         // get active page
         if ($acvtive_page == '' && isset($_GET['page'])) {
             $acvtive_page = intval($_GET['page']);
@@ -66,14 +65,18 @@ class TPagination {
 
 
         //$this->page_count = 100;
-
         // make prefix link
-        $prefix = '?' ;
-        if (isset($_GET['search'])){
-            $prefix .= 'search=' . $_GET['search'] . "&";
+        $prefix = '?';
+
+
+
+        foreach ($this->allow_in_next_pages as $get_value) {
+            if (isset($_GET[$get_value])) {
+                $prefix .= $get_value . '=' . $_GET[$get_value] . "&";
+            }
         }
 
-        $result =  '<div class="pagination">';
+        $result = '<div class="pagination">';
 
         // if greater than 1 show first and prv
         if ($acvtive_page > 1) {
@@ -92,7 +95,6 @@ class TPagination {
                 }
             }
         } else { // more than 10 page
-            
             switch ($acvtive_page) {
                 // if active page less than 7
                 case ($acvtive_page < 7):
@@ -105,25 +107,25 @@ class TPagination {
                     }
                     // show over flow
                     $result .= '<a href="overflow-last" >' . '...' . "</a> \n";
-                    $result .= '<div class="overflow" id="overflow-last">' ;
+                    $result .= '<div class="overflow" id="overflow-last">';
 
                     for ($i = $acvtive_page + 4; $i < $this->page_count; $i++) {
                         $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                     }
                     $result .= '</div>';
                     break;
-                    
-                    // if active page in last 5 page
-                case ($acvtive_page > ($this->page_count-5)):
+
+                // if active page in last 5 page
+                case ($acvtive_page > ($this->page_count - 5)):
                     // show overflows pages
-                    $result .= '<a href="overflow-first" >' . '...' . "</a> \n";   
-                    $result .= '<div class="overflow" id="overflow-first">' ;
-                    
-                    for ($i = 1; $i < ($acvtive_page-3); $i++) {
+                    $result .= '<a href="overflow-first" >' . '...' . "</a> \n";
+                    $result .= '<div class="overflow" id="overflow-first">';
+
+                    for ($i = 1; $i < ($acvtive_page - 3); $i++) {
                         $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                     }
                     $result .= '</div>';
-                    for ($i = ($acvtive_page-3); $i <= $this->page_count; $i++) {
+                    for ($i = ($acvtive_page - 3); $i <= $this->page_count; $i++) {
                         if ($i != $acvtive_page) {
                             $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                         } else {
@@ -131,32 +133,32 @@ class TPagination {
                         }
                     }
                     break;
-               
+
                 default:
                     // else not in 5 first page or 5 last page
-                    $result .= '<a href="overflow-first" >' . '...' . "</a> \n";   
-                    $result .= '<div class="overflow" id="overflow-first">' ;
-                    for ($i = 1; $i < ($acvtive_page-3); $i++) {
+                    $result .= '<a href="overflow-first" >' . '...' . "</a> \n";
+                    $result .= '<div class="overflow" id="overflow-first">';
+                    for ($i = 1; $i < ($acvtive_page - 3); $i++) {
                         $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                     }
                     $result .= '</div>';
-                    
-                    for ($i = ($acvtive_page-3); $i <= $acvtive_page + 3; $i++) {
+
+                    for ($i = ($acvtive_page - 3); $i <= $acvtive_page + 3; $i++) {
                         if ($i != $acvtive_page) {
                             $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                         } else {
                             $result .= '<a class="actived">' . $i . "</a> \n";
                         }
                     }
-                    
+
                     $result .= '<a href="overflow-last" >' . '...' . "</a> \n";
-                    $result .= '<div class="overflow" id="overflow-last">' ;
+                    $result .= '<div class="overflow" id="overflow-last">';
 
                     for ($i = $acvtive_page + 4; $i < $this->page_count; $i++) {
                         $result .= '<a href="' . $prefix . "page=" . $i . '" >' . $i . "</a> \n";
                     }
                     $result .= '</div>';
-                    
+
                     break;
             }
         }
@@ -168,11 +170,11 @@ class TPagination {
             $result .= '<a href="' . $prefix . "page=" . ($this->page_count) . '" title="' . ('آخرین') . '" >' . '&gt;&gt;' . "</a> \n";
         }
         $result .= '</div>';
-        
-        
+
+
         // result hook
         _hk('R' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $result);
-        
+
         return $result;
     }
 
