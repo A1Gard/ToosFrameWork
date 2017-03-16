a09d5c050268a7f115f1f48356194efe38758515
A1Gard <a1gard@4xmen.ir>
2017-03-16 23:59:25 +0330

    add hook to compress function [php]

--- src/libs/TCompress.php
+++ src/libs/TCompress.php
@@ -22,6 +22,8 @@ class TCompress {
      * @example $com->ZipDir('../libs', './sample.zip');
      */
     public function ZipDir($dir, $output) {
+        // Pre hook
+        _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $dir, $output);
         include_once('external/pclzip.lib.php');
         $archive = new PclZip($output);
         $v_list = $archive->create($dir, PCLZIP_OPT_REMOVE_PATH, $dir);
@@ -37,6 +39,8 @@ class TCompress {
      * @example  $com->ZipExtract('newlibs', './sample.zip');
      */
     public function ZipExtract($path, $input) {
+        // Pre hook
+        _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $path, $input);
         include_once('external/pclzip.lib.php');
         $archive = new PclZip($input);
         if ($archive->extract(PCLZIP_OPT_PATH, $path) == 0) {
