cd0392c23e3b8520764dded210ba9ce22789ced5
A1Gard <a1gard@4xmen.ir>
2017-04-23 01:50:03 +0430

    fix some bug in try log not check type [php]

--- src/libs/TTryLog.php
+++ src/libs/TTryLog.php
@@ -9,29 +9,28 @@
  * @version 1.0
  * @todo : loging try to do : login - search - send comment & ...
  */
-
 class TTryLog extends TModel {
 
     function __construct() {
         parent::__construct();
     }
-    
-    
+
     /**
      * @todo check how many tyried in last min
      * @param int $type type of try
      * @param  $last_mins in last minutes 
      * @return int how many request in test time
      */
-    public function Check($type=0, $last_mins = 10) {
+    public function Check($type = 0, $last_mins = 10) {
         // pre hook
         _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $type, $last_mins);
         $sql = "SELECT COUNT(`trylog_id`) AS 'count' FROM %table% WHERE 
             `trylog_ip` = '" . _ipi() . "' 
-             AND `trylog_time` > " . (time()- ($last_mins * 60) );
-        $result = $this->db->Select($sql, array('trylog'),array('type'=>''));
-        
-        $result_ = $result[0]['count'] ;
+             AND `trylog_time` > " . (time() - ($last_mins * 60) ) . ' AND trylog_type = :type';
+        ;
+        $result = $this->db->Select($sql, array('trylog'), array(':type' => $type));
+
+        $result_ = $result[0]['count'];
         // result hook
         _hk('R' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $result_);
         // retrun count
@@ -43,18 +42,20 @@ class TTryLog extends TModel {
      * @param int $type type of try
      * @return bool
      */
-    public function Log($type=0) {
+    public function Log($type = 0) {
         // pre hook
         _hk('P' . ':' . __CLASS__ . ':' . __FUNCTION__, $this, $type);
-        
+
         // make value
-        $data = array('type'=> 'iii',
-            'trylog_type'   => $type,
-            'trylog_ip'     => _ipi(),
-            'trylog_time'   => time());
-        
+        $data = array('type' => 'iii',
+            'trylog_type' => $type,
+            'trylog_ip' => _ipi(),
+            'trylog_time' => time());
+
         // send to insert in regiery
-        return $this->db->Insert('trylog',$data);
+        return $this->db->Insert('trylog', $data);
     }
+
 }
+
 ?>
