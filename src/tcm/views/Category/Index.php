ecb939dfc14a3662523b339b9baf574839b4cf94
A1Gard <a1gard@4xmen.ir>
2017-05-10 01:10:53 +0430

    fix some language [php]

--- src/tcm/views/Category/Index.php
+++ src/tcm/views/Category/Index.php
@@ -1,10 +1,10 @@
 <?php
 $frm = new TForm(UR_MP . 'Category/Insert', 'post', array('class' => 'form rtl'));
 
-$frm->AddField('text', 'عنوان دسته ', null, array('name' => 'category_title','class'=>'full-width'));
-$frm->AddField('select', 'مادر', null, array('name' => 'category_parent'), $this->cat->CategoryByExpect());
-$frm->AddField('textarea', 'توضیح', null, array('name' => 'category_description', "rows" => '10', 'class' => 'ckeditor'));
-$frm->AddField('submit', '', 'ارسال');
+$frm->AddField('text', _lg('title'), null, array('name' => 'category_title', 'class' => 'full-width'));
+$frm->AddField('select', _lg('Parent'), null, array('name' => 'category_parent'), $this->cat->CategoryByExpect());
+$frm->AddField('textarea', _lg('Description'), null, array('name' => 'category_description', "rows" => '10', 'class' => 'ckeditor'));
+$frm->AddField('submit', '', _lg('Submit'));
 ?>
 
 
@@ -12,30 +12,30 @@ $frm->AddField('submit', '', 'ارسال');
 
     <div class="grd12">
         <div class="margin">
-            
-        <ul class="listview">
-            <li class="list-header"> 
-                <div  class="row">
-                    <div class="grd2">ش</div>
-                    <div class="grd16"> عنوان</div>
-                    <div class="grd6">
-                        عملیات
-                    </div>
-                </div>
-            </li>
-            <?php foreach ($this->cls_list as $cls): ?>
-                <li> 
+
+            <ul class="listview">
+                <li class="list-header"> 
                     <div  class="row">
-                        <div class="grd2"><?php echo $cls['category_id'] ?></div>
-                        <div class="grd16"><?php echo $cls['category_title'] ?></div>
+                        <div class="grd2"><?php echo _lp('no.'); ?></div>
+                        <div class="grd16"> <?php echo _lp('title'); ?></div>
                         <div class="grd6">
-                            <a class="ui button delete negative" href="<?php echo UR_MP ?>Category/Delete/<?php echo $cls['category_id'] ?>"> حذف </a>
-                            <a class="ui button blue" href="<?php echo UR_MP ?>Category/Edit/<?php echo $cls['category_id'] ?>"> ویرایش </a>
+                            <?php echo _lp('Opration'); ?>
                         </div>
                     </div>
                 </li>
-            <?php endforeach; ?>
-        </ul>
+                <?php foreach ($this->cls_list as $cls): ?>
+                    <li> 
+                        <div  class="row">
+                            <div class="grd2"><?php echo $cls['category_id'] ?></div>
+                            <div class="grd16"><?php echo $cls['category_title'] ?></div>
+                            <div class="grd6">
+                                <a class="ui button delete negative" href="<?php echo UR_MP ?>Category/Delete/<?php echo $cls['category_id'] ?>"> <?php echo _lp('Delete'); ?> </a>
+                                <a class="ui button blue" href="<?php echo UR_MP ?>Category/Edit/<?php echo $cls['category_id'] ?>"> <?php echo _lp('Edit'); ?> </a>
+                            </div>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
     <div class="grd12 em-padding">
--- src/tcm/views/Manager/NewManager.php
+++ src/tcm/views/Manager/NewManager.php
@@ -1,29 +1,28 @@
 <?php
-
-global  $MANAGER_TYPE;
+global $MANAGER_TYPE;
 $frm = new TForm(UR_MP . 'Manager/Insert', 'post', array('class' => 'form'));
 
-$frm->AddField('text', 'نام کاربری', null, array('name' => 'manager_username'));
-$frm->AddField('email', 'ایمیل', null, array('name' => 'manager_email'));
-$frm->AddField('password', 'گذرواژه', null, array('name' => 'manager_password'));
-$frm->AddField('text', 'نام نمایشی', null, array('name' => 'manager_displayname'));
+$frm->AddField('text', _lg('Username'), null, array('name' => 'manager_username'));
+$frm->AddField('email', _lg('Email'), null, array('name' => 'manager_email'));
+$frm->AddField('password', _lg('password'), null, array('name' => 'manager_password'));
+$frm->AddField('text', _lg('Display name'), null, array('name' => 'manager_displayname'));
 
-$frm->AddField('select', 'نوع', null, array('name' => 'manager_type'),$MANAGER_TYPE);
-$frm->AddField('checkbox', 'محاظت شده', 1, array('name' => 'manager_protected'));
+$frm->AddField('select', _lg('Type'), null, array('name' => 'manager_type'), $MANAGER_TYPE);
+$frm->AddField('checkbox', _lg('Protected'), 1, array('name' => 'manager_protected'));
 
-$frm->AddField('submit', '', 'ارسال');
+$frm->AddField('submit', '', _lg('Submit'));
 ?>
 
-        
+
 <br />
 <h2 class="rtl">
-    <?php echo  $this->title ?>
+    <?php echo $this->title ?>
 </h2>
 <br />
 <br />
 <div class="row">
     <div class="grd-primary">
-        <?php  $frm->Render();  ?>
+        <?php $frm->Render(); ?>
     </div>
     <div class="grd-secondary">
         
