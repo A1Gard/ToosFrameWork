5b1f9237d05f2f31f27e7ef9a0e97557aa54e638
A1Gard <a1gard@4xmen.ir>
2017-05-13 01:20:04 +0430

    add some laguage translate

--- src/tcm/views/Category/Edit.php
+++ src/tcm/views/Category/Edit.php
@@ -2,18 +2,17 @@
 //var_dump($this->record);
 $frm = new TForm(UR_MP . 'Category/Update/' . $this->record['category_id'], 'post', array('class' => 'form rtl'));
 
-$frm->AddField('text', 'عنوان دسته ', $this->record['category_title'], array('name' => 'category_title', 'size' => 90));
-$frm->AddField('select', 'مادر', $this->record['category_parent'], array('name' => 'category_parent'),$this->cat->CategoryByExpect($this->record['category_id']));
-$frm->AddField('textarea', 'توضیح', $this->record['category_description'], array('name' => 'category_description', 'cols' => '90', "rows" => '10', 'class' => 'ckeditor'));
-$frm->AddField('file', 'عکس', null, array('name' => 'image'));
-$frm->AddField('submit', '', 'ارسال');
-
+$frm->AddField('text', _lg('Title'), $this->record['category_title'], array('name' => 'category_title', 'size' => 90));
+$frm->AddField('select', _lg('Parent'), $this->record['category_parent'], array('name' => 'category_parent'), $this->cat->CategoryByExpect($this->record['category_id']));
+$frm->AddField('textarea', _lg('Description'), $this->record['category_description'], array('name' => 'category_description', 'cols' => '90', "rows" => '10', 'class' => 'ckeditor'));
+$frm->AddField('file', _lg('Image'), null, array('name' => 'image'));
+$frm->AddField('submit', '', _lg('Submit'));
 ?>
 
 <br />
 <h2 class="rtl">
-<?php echo  $this->title ?>
-    <img alt="[category_image]" src="<?php echo  UR_UPLOAD ?>category/<?php echo  $this->record['category_id'] ?>.png" />
+    <?php echo $this->title ?>
+    <img alt="[category_image]" src="<?php echo UR_UPLOAD ?>category/<?php echo $this->record['category_id'] ?>.png" />
 </h2>
 <br />
 <br />
