9ebec815daf4c61082a43a4f091b13dc859ad4df
A1Gard <a1gard@4xmen.ir>
2017-06-02 23:37:22 +0430

    add vue.js  to to header for all page [php,js]

--- src/tcm/views/Layout/header.php
+++ src/tcm/views/Layout/header.php
@@ -34,6 +34,7 @@ $reg = TRegistry::GetInstance();
         <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/css/topstrap.css" />
         <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/clock-pick/jquery-clockpicker.min.css" />
 
+        <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/alertifyjs/css/alertify.min.css" />
 
         <?php if ($is_rtl): ?>
             <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/semantic-ui-rtl/semantic.rtl.min.css" />
@@ -41,7 +42,6 @@ $reg = TRegistry::GetInstance();
             <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/mpdatepicker/jquery.mpdatepicker.css" />
         <?php else: ?>
             <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/semantic-ui/semantic.min.css" />
-            <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/alertifyjs/css/alertify.min.css" />
             <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/pickadate/themes/default.css" />
             <link type="text/css" rel="stylesheet" href="<?php echo UR_MP ?>assets/libs/pickadate/themes/default.date.css" />
         <?php endif; ?>
@@ -95,13 +95,15 @@ $reg = TRegistry::GetInstance();
             var UR_BASE = "<?php echo UR_BASE ?>";
         </script>
 
+        
+        <script type="text/javascript" src="<?php echo UR_MP ?>assets/js/vue.min.js"></script>
     </head>
     <body class="<?php
     if ($reg->GetValue(ROOT_USER, 'sidebarstatus') == 0) {
         echo 'collapse-menu non-menu';
     }
     ?>"> <!--  class="collapse-menu non-menu" -->
-
+       
         <div class="ui inverted menu" id="header">
             <div class="ui container" style="max-width: none;width:100%;">
                 <a href="#" class="item" id="menu-control">  
@@ -115,24 +117,24 @@ $reg = TRegistry::GetInstance();
                     ?>
                 </a>
                 <a href="#" class="item"><?php echo _lp('Home'); ?></a>
-<!--                <div class="ui simple dropdown item">
-                    Dropdown <i class="dropdown icon"></i>
-                    <div class="menu">
-                        <a class="item" href="#">Link Item</a>
-                        <a class="item" href="#">Link Item</a>
-                        <div class="divider"></div>
-                        <div class="header">Header Item</div>
-                        <div class="item">
-                            <i class="dropdown icon"></i>
-                            Sub Menu
-                            <div class="menu">
-                                <a class="item" href="#">Link Item</a>
-                                <a class="item" href="#">Link Item</a>
-                            </div>
-                        </div>
-                        <a class="item" href="#">Link Item</a>
-                    </div>
-                </div>-->
+                <!--                <div class="ui simple dropdown item">
+                                    Dropdown <i class="dropdown icon"></i>
+                                    <div class="menu">
+                                        <a class="item" href="#">Link Item</a>
+                                        <a class="item" href="#">Link Item</a>
+                                        <div class="divider"></div>
+                                        <div class="header">Header Item</div>
+                                        <div class="item">
+                                            <i class="dropdown icon"></i>
+                                            Sub Menu
+                                            <div class="menu">
+                                                <a class="item" href="#">Link Item</a>
+                                                <a class="item" href="#">Link Item</a>
+                                            </div>
+                                        </div>
+                                        <a class="item" href="#">Link Item</a>
+                                    </div>
+                                </div>-->
             </div>
         </div>
         <div id="body-content">
