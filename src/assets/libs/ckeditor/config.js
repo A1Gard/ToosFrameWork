f4790e258ac24469a907f0b6c111e49ce43d6be9
A1Gard <a1gard@4xmen.ir>
2017-11-06 00:15:38 +0330

    update ckeditor from 4.4.1 to 4.7.2

--- src/assets/libs/ckeditor/config.js
+++ src/assets/libs/ckeditor/config.js
@@ -1,11 +1,13 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	config.language = 'fa';
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+        config.language = 'fa';
 	config.uiColor = '#13a7c7';
-	config.filebrowserUploadUrl = "/tcm/?req=Upload/RealTimeUpload" ;
+	config.filebrowserUploadUrl = "/tpanel/Upload/RealTimeUpload" ;
 };
